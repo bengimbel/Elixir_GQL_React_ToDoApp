5bcecb57cd7622d61a492d76e57481e772801dc1
Ben Gimbel <bgimbel@thecitybase.com>
2021-09-15 16:17:37 -0500

    create todo item works

--- assets/js/TodoApp/TodoList.tsx
+++ assets/js/TodoApp/TodoList.tsx
@@ -1,37 +1,44 @@
-import React from "react"
-import { useQuery, gql } from "@apollo/client"
-import { TodoItem } from "./types/TodoItem"
-import TodoListItem from "./TodoLisItem"
 
+import React, { useState } from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import { TodoItem } from "./types/TodoItem";
+import TodoListItem from "./TodoLisItem";
+import NewTodoButton from "./NewTodoButton";
+import NewTodoForm from "./NewTodoForm";
 
 interface TodoItemsQueryResult {
   todoItems: TodoItem[];
 }
-
-export const GET_TODO_ITEMS = gql`{
-  todoItems {
-    id
-    content
-    isCompleted
+export const GET_TODO_ITEMS = gql`
+  {
+    todoItems {
+      id
+      content
+      isCompleted
+    }
   }
-}
-`
-
+`;
 const TodoList = () => {
-  const { data, loading } = useQuery<TodoItemsQueryResult>(GET_TODO_ITEMS)
+  const { data, loading } = useQuery<TodoItemsQueryResult>(GET_TODO_ITEMS);
+  const [showForm, setShowForm] = useState(false);
 
   return (
     <div className="todo_list">
-      <h3 className="todo_list__header">
-        To Do Items
-      </h3>
+      <h3 className="todo_list__header">Todo Items</h3>
       <div className="todo_list__list">
+        {loading && <div>Loading...</div>}
         {data?.todoItems?.map((item: TodoItem) => (
           <TodoListItem key={item.id} {...item} />
         ))}
+        {showForm ? <NewTodoForm /> : null}
       </div>
+      <div className="todo_list__spacer"></div>
+      <footer className="todo_list__footer">
+        <NewTodoButton onClick={() => setShowForm(true)} />
+      </footer>
     </div>
-  )
-}
+  );
+};
 
 export default TodoList;
